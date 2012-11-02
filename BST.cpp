9f4475c4160fcc3adace3a4cbcb1de7aae19d0b6
Jason Blanchard <jasonmblanchard@gmail.com>
2012-11-02 01:30:23 -0400

    Remove function added to BST.

--- BST.cpp
+++ BST.cpp
@@ -1,5 +1,6 @@
 #include "BST.h"
 #include <iostream>
+#include <assert.h>
 
 template <typename T>
 BST<T>::BST() {
@@ -36,8 +37,98 @@ void BST<T>::insert(T v) {
 
 template <typename T>
 void BST<T>::remove(T v) {
-  Node<T>* temp = new Node<T>(v);
-  root = temp;
+    assert(root != 0);
+    Node<T> *toDelete = root;
+    Node<T> *prev = 0;
+    bool left = false;
+
+    // find node
+    while (toDelete != 0 && toDelete->getValue() != v) {
+        prev = toDelete;
+        if (toDelete->getValue() < v) {
+            toDelete = toDelete->getRightChild();
+            left = false;
+        }
+        else {
+            toDelete = toDelete->getLeftChild();
+            left = true;
+        }
+    }
+
+    if (toDelete == 0) // return if not found
+        return;
+
+    if (toDelete->getLeftChild() == 0
+            && toDelete->getRightChild() == 0) { // no children? 
+        if (prev == 0) {
+            root = 0;
+        } else {
+            if (left) {
+                prev->setLeftChild(*(toDelete->getRightChild()));
+            } else {
+                prev->setRightChild(*(toDelete->getRightChild()));
+            }
+        }
+
+        delete toDelete;
+    } else if ((toDelete->getLeftChild() != 0) 
+            != (toDelete->getRightChild() != 0)) { // one child?
+
+        if (toDelete->getLeftChild() != 0) { // left child exists
+            if (prev == 0) { // toDelete is root
+                root = toDelete->getLeftChild();
+            } else { // toDelete is not root
+                if (left) {
+                    prev->setLeftChild(*(toDelete->getLeftChild()));
+                } else {
+                    prev->setRightChild(*(toDelete->getLeftChild()));
+                }
+            }
+        } else { // right child exists
+            if (prev == 0) {
+                root = toDelete->getRightChild();
+            } else {
+                if (left) {
+                    prev->setLeftChild(*(toDelete->getRightChild()));
+                } else {
+                    prev->setRightChild(*(toDelete->getRightChild()));
+                }
+            }
+        }
+
+        delete toDelete;
+    } else { // find inorder predecessor (left then right all the way down)
+        Node<T> *parent_iop = toDelete;
+        Node<T> *iop = toDelete->getLeftChild();
+
+        while (iop->getRightChild() != 0) {
+            parent_iop = iop;
+            iop = iop->getRightChild();
+        }
+
+        iop->setRightChild(*(toDelete->getRightChild()));
+
+        if (parent_iop == toDelete) {
+        } else {
+            parent_iop->setRightChild(*(iop->getLeftChild()));
+        }
+        
+        if (iop != toDelete->getLeftChild()) {
+            iop->setLeftChild(*(toDelete->getLeftChild()));
+        }
+
+        if (prev == 0) {
+            root = iop;
+        } else {
+            if (left) {
+                prev->setLeftChild(*iop);
+            } else {
+                prev->setRightChild(*iop);
+            }
+        }
+
+        delete toDelete;
+    }
 }
 
 template <typename T>
--- tree_test.cpp
+++ tree_test.cpp
@@ -1,15 +1,25 @@
 #include "BST.h"
+#include <iostream>
 
 int main() {
   BST<int>* bst = new BST<int>();
 
-  bst->insert(5);
+  bst->insert(10);
   bst->insert(6);
   bst->insert(3);
   bst->insert(2);
   bst->insert(7);
-  //bst->insert(12);
+  bst->insert(12);
+  bst->print();
 
+  std::cout << "\n";
+  bst->remove(6);
+  bst->print();
+  std::cout << "\n";
+  bst->remove(3);
+  bst->print();
+  std::cout << "\n";
+  bst->remove(10);
 
   bst->print();
 
